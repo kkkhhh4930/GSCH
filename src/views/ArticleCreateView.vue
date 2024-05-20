44255cf99b1fda6926f1dd4cb010fc223c53e246
yjjj1612 <yjjj1612@daum.net>
2024-05-20 16:47:24 +0900

    게시판 CRUD구현

--- src/views/ArticleCreateView.vue
+++ src/views/ArticleCreateView.vue
@@ -1,56 +1,60 @@
 <template>
-    <div>
-      <h1>게시글 작성</h1>
-      <form @submit.prevent="createArticle">
-        <div>
-          <label for="title">제목 : </label>
-          <input type="text" v-model.trim="title" id="title">
-        </div>
-        <div>
-          <label for="content">내용 : </label>
-          <textarea v-model.trim="content" id="content"></textarea>
-        </div>
-        <input type="submit">
-      </form>
-    </div>
-  </template>
-  
-  <script setup>
-  import axios from 'axios'
-  import { ref } from 'vue'
-  import { useArticleStore } from '@/stores/articles'
-  import { useRouter } from 'vue-router'
-  
-  const store = useArticleStore()
-  const title = ref(null)
-  const content = ref(null)
-  const router = useRouter()
-  
-  console.group(store.API_URL)
-  const createArticle = function () {
-    axios({
-      method: 'post',
-      url: `${store.API_URL}/community/articles/`,
-      data: {
-        title: title.value,
-        content: content.value
-      },
-      headers: {
-        Authorization: `Token ${store.token}`
-      }
+  <!-- 게시글 작성 폼 -->
+  <div>
+    <h1>게시글 작성</h1>
+    <form @submit.prevent="createArticle">
+      <!-- 제목 입력 -->
+      <div>
+        <label for="title">제목 : </label>
+        <input type="text" v-model.trim="title" id="title">
+      </div>
+      <!-- 내용 입력 -->
+      <div>
+        <label for="content">내용 : </label>
+        <textarea v-model.trim="content" id="content"></textarea>
+      </div>
+      <!-- 제출 버튼 -->
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { useArticleStore } from '@/stores/articles'
+import { useRouter } from 'vue-router'
+
+const store = useArticleStore()
+const title = ref(null)
+const content = ref(null)
+const router = useRouter()
+
+// 게시글 생성 함수
+const createArticle = function () {
+  axios({
+    method: 'post',
+    url: `${store.API_URL}/community/articles/`,
+    data: {
+      title: title.value,
+      content: content.value
+    },
+    headers: {
+      Authorization: `Token ${store.token}`
+    }
+  })
+    .then((response) => {
+      console.log(response.data)
+      // 게시글 목록 페이지로 이동
+      router.push({ name: 'community' })
     })
-      .then((response) => {
-        console.log(response.data)
-        router.push({ name: 'community' })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-  
-  </script>
-  
-  <style>
-  
-  </style>
-  
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+</script>
+
+<style>
+/* 스타일 정의 */
+</style>
--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,29 +1,50 @@
 <template>
-    <div>
-      <h1>커뮤니티 페이지</h1>
-      <RouterLink :to="{ name: 'articleCreate' }">
-        [작성하기]
-      </RouterLink>
-      <hr>
-      <ArticleList />
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted } from 'vue'
-  import { useArticleStore } from '@/stores/articles'
-  import { RouterLink } from 'vue-router'
-  import ArticleList from '@/components/ArticleList.vue'
-  
-  const store = useArticleStore()
-  
-  onMounted(() => {
-    store.getArticles()
-  })
-  
-  </script>
-  
-  <style>
-  
-  </style>
-  
+  <!-- 커뮤니티 페이지 -->
+  <div>
+    <h1>커뮤니티 페이지</h1>
+    <!-- 글 작성하기 링크 -->
+    <RouterLink :to="{ name: 'articleCreate' }">
+      [작성하기]
+    </RouterLink>
+    <!-- 게시글 목록 -->
+    <ul>
+      <div
+        v-for="post in store.postList"
+        :key="post.pk"
+        :post="post"
+        @click="goDetail(post.pk)"
+      >
+        <p>{{ post.category.name }}</p>
+        <h3> <span>{{ post.pk }}번 글 |</span>  {{ post.title }}</h3>
+        <p>작성자 : {{ post.user.username }}</p>
+        <hr>
+      </div>
+    </ul>
+    <!-- ArticleList 컴포넌트 -->
+    <ArticleList />
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useArticleStore } from '@/stores/articles'
+import { RouterLink } from 'vue-router'
+import ArticleList from '@/components/ArticleList.vue'
+
+const store = useArticleStore()
+
+// 컴포넌트가 마운트될 때 게시글 목록을 가져오는 함수 호출
+onMounted(() => {
+  store.getArticles()
+})
+
+// 게시글 상세 페이지로 이동하는 함수
+const goDetail = (pk) => {
+  router.push({name:'articleDetail', params:{pk: pk}})
+}
+
+</script>
+
+<style>
+/* 스타일 정의 */
+</style>
